d9ebca91549fc7cd6a9f58f752835994a94e7ed6
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-26 13:17:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('button').addEventListener('click', calculate)
+
+let numb1 = 0
+let numb2 = 0
+let count = 0
+let answer = 0
+
+function calculate () {
+  answer = 0
+  numb1 = document.getElementById('num').value
+  numb2 = document.getElementById('num1').value
+  numb1 = parseInt(numb1)
+  numb2 = parseInt(numb2)
+
+ for (count = 0; count < numb1; count++)  
+ {
+   answer = answer + numb2
+ }
+ document.getElementById('answer').innerHTML = answer
+}
